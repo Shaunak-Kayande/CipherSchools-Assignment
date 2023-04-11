5fe932734167f04f34a1b21f340589505d72baa6
Shaunak-Kayande <shaunakk.2801@gmail.com>
2023-04-11 16:12:18 +0530

    NavBar
    Completed Nav Bar Design

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <div className="font-sans bg-gray-100">
+      <Header />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
